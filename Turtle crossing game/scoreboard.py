a8e301dc2fbbdfb6be9a39a9514c995155db71cd
romeo-katanasho <romeokatanasho557@gmail.com>
2025-11-11 13:44:02 +0200

    Add files via upload

---
+++ Turtle crossing game/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-260, 260)
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"level:{self.level}", align="left", font= FONT)
+    def increase_leve(self):
+        self.level +=1
+        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font= FONT)
+
+
